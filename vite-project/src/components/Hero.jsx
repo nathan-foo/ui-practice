6a248780143a62426d03aafc221a87c0ecddc0c7
Nathan Foo <ntfoo27@outlook.com>
2024-11-27 00:09:26 -0600

    Implement navbar

--- vite-project/src/components/Hero.jsx
+++ vite-project/src/components/Hero.jsx
@@ -77,7 +77,7 @@ const Hero = () => {
 
     return (
         <div className='relative h-dvh w-screen overflow-x-hidden'>
-            {isLoading &&   (
+            {isLoading && (
                 <div className='flex-center absolute z-[100] h-dvh w-screen overflow-hidden bg-violet-50'>
                     <div className='three-body'>
                         <div className='three-body__dot' />
